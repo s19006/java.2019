5f81455bc9e941bc032b477afb6658cb15829db0
aoi <s19006@std.it-college.ac.jp>
2019-11-29 13:15:38 +0900

    課題

--- test/Ex4/Ex4.java
+++ test/Ex4/Ex4.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Comparator;
 class Ex4{
 
 	public int [] ex4_1(int [] a){
@@ -14,63 +15,109 @@ class Ex4{
     }
 
 	public int [] ex4_2(int [] a){
-        int[] z = new int[a.length];
-     for (int i = 0 ; i < z.length -1; i++) {
-         z[i] = a[i];
-     }
-     Collections.reverse(z);
-		return z;
 
+ List<Integer> list = new ArrayList<Integer>();
+        for (int tmp : a) {
+            list.add((Integer)tmp);
+        }
+
+        Collections.sort(list, Comparator.reverseOrder());
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = list.get(i);
+        }
+        return a;
 	}
 
 	public String [] ex4_3(int [] a){
-		
-		String [] hantei = {"dummy"};
-		
-		return hantei;
 
+        String [] ret = new String[a.length];
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] > 0) {
+                ret[i] = "正";
+            } else if (a[i] < 0) {
+                ret[i] = "負";
+            } else {
+                ret[i] = "零";
+            }
+        }
+         return ret;
 	}
 
 	public int ex4_4(int a){
-		
-		return -1;
+
+       int val = a;
+       int valLen = String.valueOf(val).length();
+
+       return valLen;
 
 	}
 
 	public int ex4_5(int a){
-		
-		return -1;
 
-	}
+        int sum = 0;
+        for (int i = 1; i <= a; i++){
+            sum += i;
+        }
+        return sum;
+    }
+
 
 	public int [] ex4_6(int a){
-		
-		int [] ret = new int[1];
-		
-		return ret;
 
+        List<Integer> b = new ArrayList<Integer>();
+
+        for (int i = 1; i <= a; i++) {
+            if (a % i == 0) {
+                b.add(i);
+            }
+        }
+
+		int [] ret = new int[b.size()];
+
+        for (int i = 0; i < ret.length; i++) {
+            ret[i] = b.get(i);
+        }
+		return ret;
 	}
 
 	public boolean ex4_7(int a){
-		
-		return false;
 
+
+        if (a == 1) {
+            return false;
+        }
+
+        for (int i = 2; i < a; i++) {
+            if (a % i == 0) {
+                return false;
+            }
+        }
+        return true;
 	}
 
 	public int ex4_8(int [] a){
-		
-		int ret = 0;
-		
-		return ret;
 
-	}
+		int sum = 0;
+
+        for (int i : a){
+            sum += i;
+        }
+		return sum;
+
+    }
 
 	public int ex4_9(int [] a){
-		
-		int ret = 0;
-		
-		return ret;
 
+        float sum = 0;
+
+        for (int i : a) {
+            sum += i;
+        }
+
+        int ret = Math.round(sum/a.length);
+
+        return ret;
 	}
 }
 
